2d496d72a43d6c018539b2513d82a9d97967d539
carljgu@stud.ntnu.no <cjgutzkow@gmail.com>
2022-04-03 12:48:48 +0200

    to norwegian

--- r8/builtin_challenges/enigma.py
+++ r8/builtin_challenges/enigma.py
@@ -11,8 +11,9 @@ class Enigma(r8.Challenge):
 
     async def description(self, user: str, solved: bool):
         return r8.util.media(None, """
-            <h6>What does this message say?</h6>
+            <h6>Hva sier denne beskjeden?</h6>
             <p>kq tv qzamamau zh exhafg q zngzrj xopgsuw ryhed dle og gmlq hu wicnjbs dkq qyahymxiuz wgjjvqxu</p>
+            <a href="/static/tcp-server.svg" download>Link</a>
             <form>
                 <input class="form-control mb-1" name="message" type="text"/>
                 <button class="btn btn-primary mb-1">Submit</button>
@@ -22,7 +23,7 @@ class Enigma(r8.Challenge):
 
     async def handle_post_request(self, user: str, request: web.Request):
         json = await request.json()
-        if json.get("message", "") == "It is possible to invent a single machine which can be used to compute any computable sequence":
+        if json.get("message", "").lower().strip() == "it is possible to invent a single machine which can be used to compute any computable sequence":
             return self.log_and_create_flag(request, user)
         else:
-            return web.HTTPBadRequest(reason="I guess the encryption is very good if you can't crack it.")
+            return web.HTTPBadRequest(reason="Stemmer ikke. Må være en bra kryptering om du ikke klarer det.")
