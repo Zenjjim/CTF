052f68f079d9ccddb7628dafed231e011d915a37
carljgu@stud.ntnu.no <cjgutzkow@gmail.com>
2022-04-06 17:56:05 +0200

    made flag encrypted instead

---
+++ r8/builtin_challenges/decryption/configurations.py
@@ -0,0 +1,37 @@
+configs = [
+    ("II V I", "HEC", "AH DF NV TE WQ ZG", "NJQ"),
+    ("V III IV", "ZZD", "AO CT HG QM XF YP", "DIS"),
+    ("II IV I", "PDA", "AD BW LO NZ QK XE", "ABA"),
+    ("I III V", "WXG", "CP EK HS IO QZ WB", "ENG"),
+    ("IV III V", "ZPH", "AB MF OJ QG WE YC", "FGC"),
+    ("I II IV", "EHH", "CK FH GL JS QN ZI", "RHP"),
+    ("I III V", "IEF", "BA CL FT HM KN UY", "EMP"),
+    ("II III V", "HDO", "AK BW ER OT UN ZF", "RVZ"),
+    ("V II III", "JCS", "DK FS PG QM UA WZ", "BDB"),
+    ("II III V", "MXC", "EF PB TQ VM YH ZK", "NLK"),
+    ("I IV II", "PFQ", "FL JV PA TQ XI YM", "IBI"),
+    ("V I II", "XYB", "JC KD MR UQ YE ZX", "EBG"),
+    ("IV III I", "BVV", "BW JX NR PG UT ZQ", "KTJ"),
+    ("IV I V", "TRD", "CT DG JU NE PB WY", "HOR"),
+    ("IV II V", "VTB", "DO HG IU NV RP YX", "PQN"),
+    ("III IV II", "NYK", "EK JR LS QM VP ZW", "LBX"),
+    ("I II V", "MTA", "AS FQ IG KC OR YM", "GWP"),
+    ("I II V", "MIM", "BO HY IN PF RD XL", "CEV"),
+    ("II I V", "ATN", "DB KY PT RQ SJ VL", "IYC"),
+    ("IV III V", "IKP", "AD JH NM RQ SZ TG", "QHA"),
+    ("IV V I", "NXS", "GP MS OB RF VZ XJ", "YUR"),
+    ("I V IV", "QKS", "AM JE KB QN TF UG", "MKX"),
+    ("V II I", "GQN", "FV ID PZ TJ WQ YG", "QHP"),
+    ("II I III", "UTJ", "CI KN OA QX UV ZR", "WVH"),
+    ("I IV III", "ZWV", "EC IJ MX NU SZ TH", "KLR"),
+    ("V III II", "MLT", "FR JZ KG LS QW UH", "NYO"),
+    ("II I IV", "LZP", "DB FI GC HQ JZ WE", "YZQ"),
+    ("III IV I", "PRC", "FL KH WI XB YP ZV", "JST"),
+    ("III I II", "HVH", "FG RC SK UT VI WH", "JFM"),
+    ("II III IV", "EFK", "HS KR LG NU WY XB", "KTY")
+]
+
+
+
+def get_config(day):
+    return configs[day - 1]
